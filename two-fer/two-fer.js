c886556ceb063e2e0d5fe062a1dbd95596292bd2
Sergio Alves <salves2383@gmail.com>
2018-03-01 18:54:27 +0100

    Second kata javascript

---
+++ two-fer/two-fer.js
@@ -0,0 +1,13 @@
+var TwoFer = function () {};
+
+TwoFer.prototype.twoFer = function (who) {
+  // your code goes here
+  // You will have to use the parameter who
+  // in some way. In this example, it is just
+  // returned, but your solution will have to
+  // use a conditional.
+  return who === undefined ? "One for you, one for me." : `One for ${who}, one for me.`;
+
+};
+
+module.exports = TwoFer;
