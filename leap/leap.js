c28670323926c2ff0d6bd8577a9cc05998196e50
Sergio Alves <salves2383@gmail.com>
2018-03-07 17:24:12 +0100

    Added solution for leap year with javascript

---
+++ leap/leap.js
@@ -0,0 +1,22 @@
+//
+// This is only a SKELETON file for the "Leap" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Year = function (year) {
+  this.value = year;
+};
+
+Year.prototype.isLeap = function () {
+  if(this.value % 4 === 0) {
+    if(this.value % 100 === 0) {
+      return this.value % 400 === 0 ? true : false;
+    } else {
+      return true;
+    }
+  } else {
+    return false;
+  }
+};
+
+module.exports = Year;
